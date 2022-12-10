50692f48537009a3e285e5bb25c9703221aa378d
kranthikumar2804 <119719624+kranthikumar2804@users.noreply.github.com>
2022-12-10 10:25:14 +0530

    #include <stdio.h> #define N 3 int main() {     int a[N];     int i,d;     int c=1;     scanf("%d%d%d",&a[0],&a[1],&a[2]);     for (i=0;i<N;i++)     {         c=c*a[i];         d=(512*c*2)/1024;     }     printf("%dKB",d); }

---
+++ Find_the_Capacity.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define N 3
+int main()
+{
+    int a[N];
+    int i,d;
+    int c=1;
+    scanf("%d%d%d",&a[0],&a[1],&a[2]);
+    for (i=0;i<N;i++)
+    {
+        c=c*a[i];
+        d=(512*c*2)/1024;
+    }
+    printf("%dKB",d);
+}
