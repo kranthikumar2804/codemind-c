5ffa2575f631d0b2c50fe1d8b68d225fd28a180e
kranthikumar2804 <119719624+kranthikumar2804@users.noreply.github.com>
2023-01-03 18:42:48 +0530

    #include <stdio.h> int main() {     int num1, num2;     scanf("%d %d", &num1, &num2);     if(num1 > num2)     {         printf("%d ", num1);     }     else if (num2 > num1)     {         printf("%d ", num2);     }     else     {         printf("Both are Equal ");     }     return 0; }

---
+++ Biggest_of_Two_Numbers.c
@@ -0,0 +1,22 @@
+ #include <stdio.h>
+int main()
+{
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    if(num1 > num2)
+    {
+        printf("%d
+", num1);
+    }
+    else if (num2 > num1)
+    {
+        printf("%d
+", num2);
+    }
+    else
+    {
+        printf("Both are Equal
+");
+    }
+    return 0;
+}
